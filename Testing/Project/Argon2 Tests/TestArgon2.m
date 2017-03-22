206628f58aa32b53e4c6c55350755c8f0ab74f4e
CodingMarkus <26418089+CodingMarkus@users.noreply.github.com>
2017-03-22 13:04:43 +0100

    Improved UnitTest code a bit

--- Testing/Project/Argon2 Tests/TestArgon2.m
+++ Testing/Project/Argon2 Tests/TestArgon2.m
@@ -6,33 +6,6 @@
 
 #import "Argon2KeyDerivator.h"
 
-#define OUT_LEN 32
-
-
-//    char encoded[ENCODED_LEN];
-//    int ret, i;
-//
-//    ret = argon2_hash(t, 1 << m, p, pwd, strlen(pwd), salt, strlen(salt), out,
-//                      OUT_LEN, encoded, ENCODED_LEN, Argon2_i, version);
-//    assert(ret == ARGON2_OK);
-//
-//    for (i = 0; i < OUT_LEN; ++i)
-//        sprintf((char *)(hex_out + i * 2), "%02x", out[i]);
-//
-//    assert(memcmp(hex_out, hexref, OUT_LEN * 2) == 0);
-//
-//    if (ARGON2_VERSION_NUMBER == version) {
-//        assert(memcmp(encoded, mcfref, strlen(mcfref)) == 0);
-//    }
-//
-//    ret = argon2_verify(encoded, pwd, strlen(pwd), Argon2_i);
-//    assert(ret == ARGON2_OK);
-//    ret = argon2_verify(mcfref, pwd, strlen(pwd), Argon2_i);
-//    assert(ret == ARGON2_OK);
-//
-//    printf("PASS\n");
-
-
 @interface TestArgon2 : XCTestCase
 
 @end
@@ -41,15 +14,37 @@
 @implementation TestArgon2
 
 - (void)test {
-
-#	define hashtest(v0, t0, m0, p0, pwd0, salt0, hexref0, mcfref0) \
-		[self t:t0 m:m0 p:p0 pwd:pwd0 salt:salt0 hexref:hexref0 mcfref:mcfref0]
+#	define OUT_LEN 32
+#	define hashtest(v0, t0, m0, p0, pwd0, salt0, hexref0, mcfref0)            \
+	do {                                                                      \
+		NSError * error = nil;                                                \
+		NSData * rawPwd = [NSData dataWithBytes:pwd0 length:strlen(pwd0)];    \
+		NSData * rawSalt = [NSData dataWithBytes:salt0 length:strlen(salt0)]; \
+		NSData * result = [Argon2KeyDerivator makeKeyOfLength:OUT_LEN         \
+			usingType:Argon2i rounds:t0 memory:1 << m0 threads:p0             \
+			password:rawPwd salt:rawSalt outError:&error                      \
+		];                                                                    \
+		XCTAssertNotNil(result);                                              \
+		if (result) XCTAssertNil(error);                                      \
+		if (result && !error) {                                               \
+			const uint8_t * outPtr = result.bytes;                            \
+			unsigned char hex_out[(OUT_LEN * 2) + 4] = { 0 };                 \
+			for (int i = 0; i < OUT_LEN; i++) {                               \
+				sprintf((char *)(hex_out + (i * 2)), "%02x", outPtr[i]);      \
+			}                                                                 \
+			XCTAssert(memcmp(hex_out, hexref0, OUT_LEN * 2) == 0);            \
+		}                                                                     \
+	} while(0)
 
 	// Original reference vectors follow below:
     hashtest(version, 2, 16, 1, "password", "somesalt",
              "c1628832147d9720c5bd1cfd61367078729f6dfb6f8fea9ff98158e0d7816ed0",
              "$argon2i$v=19$m=65536,t=2,p=1$c29tZXNhbHQ"
              "$wWKIMhR9lyDFvRz9YTZweHKfbftvj+qf+YFY4NeBbtA");
+    hashtest(version, 2, 20, 1, "password", "somesalt",  
+             "d1587aca0922c3b5d6a83edab31bee3c4ebaef342ed6127a55d19b2351ad1f41",
+             "$argon2i$v=19$m=1048576,t=2,p=1$c29tZXNhbHQ"  
+             "$0Vh6ygkiw7XWqD7asxvuPE667zQu1hJ6VdGbI1GtH0E");
     hashtest(version, 2, 18, 1, "password", "somesalt",
              "296dbae80b807cdceaad44ae741b506f14db0959267b183b118f9b24229bc7cb",
              "$argon2i$v=19$m=262144,t=2,p=1$c29tZXNhbHQ"
@@ -78,29 +73,6 @@
              "b0357cccfbef91f3860b0dba447b2348cbefecadaf990abfe9cc40726c521271",
              "$argon2i$v=19$m=65536,t=2,p=1$ZGlmZnNhbHQ"
              "$sDV8zPvvkfOGCw26RHsjSMvv7K2vmQq/6cxAcmxSEnE");
-}
-
-
-- (void)t:(uint32_t)t m:(uint32_t)m p:(uint32_t)p
-	pwd:(char *)pwd salt:(char *)salt
-	hexref:(char *)hexref mcfref:(char *)mcfref
-{
-	NSError * error = nil;
-	NSData * rawPwd = [NSData dataWithBytes:pwd length:strlen(pwd)];
-	NSData * rawSalt = [NSData dataWithBytes:salt length:strlen(salt)];
-	NSData * result = [Argon2KeyDerivator makeKeyOfLength:OUT_LEN
-		usingType:Argon2i rounds:t memory:1 << m threads:p
-		password:rawPwd salt:rawSalt outError:&error
-	];
-	XCTAssertNotNil(result);
-	if (!result) return;
-
-	const uint8_t * outPtr = result.bytes;
-	unsigned char hex_out[(OUT_LEN * 2) + 4] = { 0 };
-	for (int i = 0; i < OUT_LEN; i++) {
-		sprintf((char *)(hex_out + (i * 2)), "%02x", outPtr[i]);
-	}
-	XCTAssert(memcmp(hex_out, hexref, OUT_LEN * 2) == 0);
 }
 
 @end
